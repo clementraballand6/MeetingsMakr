f03cb24b0375f971fd574bfffc33b24f8db46b86
clément <clement.raballand6@gmail.com>
2017-03-13 19:19:33 +0100

    modif came case bdd

--- srv/mysql.js
+++ srv/mysql.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'meetingsmakr'
+    user     : 'dev',
+    password : 'pwsio',
+    database : 'MeetingsMakr'
 });
 
 connection.connect();
